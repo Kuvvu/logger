7bffb0aff9aa9f9a62c003df68db28d6e75620e5
Simon Jenny <simon.jenny@me.com>
2023-09-18 08:21:14 +0200

    Update Logger.php

--- src/Logger.php
+++ src/Logger.php
@@ -6,7 +6,7 @@ use Monolog\Logger as _Logger;
 
 class Logger {
 
-    public function __invoke(array $config): Logger 
+    public function __invoke(array $config): _Logger 
     {
         return new _Logger('logger', [
             new LoggerHandler()
