ca38b1834e86969168b825208e46a3a203bb888a
Simon Jenny <simon.jenny@me.com>
2023-09-23 17:49:44 +0200

    Update logger.php

--- config/logger.php
+++ config/logger.php
@@ -4,7 +4,7 @@ return [
 
     /* Create a copy/backup of current env.example at start */
 
-    'endpoint' => env('KUVVU_LOG_URL', 'https://dev.kuvvu.ch/api/log'),
+    'endpoint' => env('KUVVU_LOG_URL', 'https://log.kuvvu.ch/api/log'),
     'project'  => env('APP_URL')
   
 ];
